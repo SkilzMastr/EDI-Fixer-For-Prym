aca5e47a8a8d2156d92935027c948c1b59062ad8
SkilzMastr <64101555+SkilzMastr@users.noreply.github.com>
2020-07-29 22:04:56 -0400

    Added Everything

---
+++ main.py
@@ -0,0 +1,20 @@
+import os
+from time import sleep
+
+DIR = os.getcwd() 
+LIST = os.listdir(fr'{DIR}\filesIncorrect')
+DIR_INCORRECT = fr'{DIR}\filesIncorrect'
+DIR_CORRECT = fr'{DIR}\filesCorrect'
+
+for files in LIST:
+    fin = open(fr'{DIR_INCORRECT}\{files}', "rt")
+    fout = open(fr'{DIR_CORRECT}\{files}', "wt")
+
+    for line in fin:
+        fout.write(line.replace('*>*', '*:*'))
+    fin.close()
+    fout.close()
+
+
+sleep(30)
+print('DO NOT RUN AGAIN THE PROGRAM WORKED. CORRECT FILES ARE IN filesCorrect.')
